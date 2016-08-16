f24de0b68429dcae8efd4664d16532168a4dc8e9
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-08-16 20:21:10 +0100

    Edit & Remove Posts

--- client/templates/admin/posts/posts.js
+++ client/templates/admin/posts/posts.js
@@ -9,10 +9,41 @@ Template.add_post.events({
             body: body
         });
 
-        FlashMessages.sendSuccess('Post has been successfully added!');
+        FlashMessages.sendSuccess('Post successfully added!');
         Router.go('/admin/posts');
 
         // Prevent submit
         return false;
+    }
+});
+
+Template.edit_post.events({
+    'submit .edit_post_form': function(event) {
+        var title = event.target.title.value;
+        var body = event.target.body.value;
+
+        // Update the post
+        Posts.update({
+            _id: this._id
+            }, {
+                $set: {title: title, body: body}
+        });
+
+        FlashMessages.sendSuccess('Post successfully updated!');
+        Router.go('/admin/posts');
+
+        // Prevent submit
+        return false;
+    }
+});
+
+Template.list_posts.events({
+    'click .delete_post': function(event) {
+        if (confirm('Do you really want to delete "' + this.title + '" post?')) {
+            Posts.remove(this._id);
+            FlashMessages.sendSuccess('Post successfully deleted!');
+
+            return false;
+        }
     }
 });
